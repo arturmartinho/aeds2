10571bdd7b571737ab16178b26a978360e2d2999
artur martinho <142224373+arturmartinho@users.noreply.github.com>
2026-08-28 18:39:44 -0300

    Rewrite Inversao from Java to C
    Converted Java Inversao class to C, implementing string inversion.

--- tp1/Inversao.c
+++ tp1/Inversao.c
@@ -1,18 +1,28 @@
-public class Inversao {
-    public static String inverter(String s) {
-        String resp = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            resp += s.charAt(i);
+#include <stdio.h>
+#include <string.h>
+
+void inverter(const char *s, char *resp) {
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+        resp[i] = s[len - 1 - i];
+    }
+    resp[len] = '\0';
+}
+
+int main() {
+    char linha[1000];
+    char resp[1000];
+
+    while (fgets(linha, sizeof(linha), stdin) != NULL) {
+        linha[strcspn(linha, "\n")] = '\0';
+
+        if (strcmp(linha, "FIM") == 0) {
+            break;
         }
-        return resp;
+
+        inverter(linha, resp);
+        printf("%s\n", resp);
     }
 
-    public static void main(String[] args) {
-        MyIO.setCharset("UTF-8");
-        String linha = MyIO.readLine();
-        while (!linha.equals("FIM")) {
-            MyIO.println(inverter(linha));
-            linha = MyIO.readLine();
-        }
-    }
+    return 0;
 }
